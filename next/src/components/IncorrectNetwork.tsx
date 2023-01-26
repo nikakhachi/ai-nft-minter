1afecf57c16fea1f33bcf3cc83b20d6729d89d74
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-01-26 11:44:27 +0400

    styling to wallet-not-connected and incorrect-network components

--- next/src/components/IncorrectNetwork.tsx
+++ next/src/components/IncorrectNetwork.tsx
@@ -1,10 +1,24 @@
 const IncorrectNetwork = () => {
   return (
-    <>
-      <p className="text-white text-center mt-14 text-3xl">
-        Please switch the network to <span className="underline italic">GOERLI</span> and refresh this page
-      </p>
-    </>
+    <div className="container mx-auto mt-2 pb-14">
+      <div className="flex w-full justify-center flex-col">
+        <div className="flex flex-col items-center justify-center">
+          <img src="mona.png" />
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-center text-white text-xl md:text-3xl font-[Courier] my-5 md:my-12">
+            Please switch the network to <span className="underline italic">GOERLI</span> and refresh this page
+          </p>
+          <button
+            onClick={() => window.location.reload()}
+            type="submit"
+            className="mt-12 w-96 border-2 border-[#fe5cb8] text-white font-[Courier] duration-500 px-6 py-2 rounded flex justify-center items-center h-10"
+          >
+            Refresh
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- next/src/components/wallet-not-connected/index.tsx
+++ next/src/components/wallet-not-connected/index.tsx
@@ -5,13 +5,27 @@ const WalletNotConnected = () => {
   const nftCollectionContext = useContext(NFTCollectionContext);
 
   return (
-    <button
-      onClick={nftCollectionContext?.connectToWallet}
-      type="submit"
-      className="m-auto mt-10 w-96 border-2 border-[#fe5cb8] text-white font-[Courier] duration-500 px-6 py-2 rounded flex justify-center items-center h-10"
-    >
-      Connect the Wallet
-    </button>
+    <div className="container mx-auto md:mt-24 pb-14">
+      <div className="flex w-full justify-center flex-col lg:flex-row">
+        <div className="flex flex-col items-center justify-center">
+          <img src="mona.png" />
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-center text-white text-xl md:text-3xl font-[Courier]">Connect your Metamask Wallet,</p>
+          <p className="text-center text-white text-xl md:text-3xl font-[Courier] my-5 md:my-12">
+            Generate an image based on your input with AI,
+          </p>
+          <p className="text-center text-white text-xl md:text-3xl font-[Courier]">Mint the image as an NFT and own it</p>
+          <button
+            onClick={nftCollectionContext?.connectToWallet}
+            type="submit"
+            className="mt-12 w-96 border-2 border-[#fe5cb8] text-white font-[Courier] duration-500 px-6 py-2 rounded flex justify-center items-center h-10"
+          >
+            Connect the Wallet
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
