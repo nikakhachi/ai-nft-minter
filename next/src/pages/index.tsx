dff37535f7c2ae9fb3fd279acc6f52b14ce627a2
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-05-22 21:00:18 +0400

    fetch nfts from provider from serversideprops

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -1,14 +1,18 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import { useContext } from "react";
-import { NFTCollectionContext } from "@/contexts/NFTCollectionContext";
+import { NFTCollectionContext, NftType } from "@/contexts/NFTCollectionContext";
 import NavBar from "@/components/NavBar";
 import WalletNotConnected from "@/components/wallet-not-connected";
 import WalletConnected from "@/components/wallet-connected";
 import Loading from "@/components/Loading";
 import IncorrectNetwork from "@/components/IncorrectNetwork";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { jsonRpsServices } from "@/services/jsonRpc.service";
+import { ipfsUriToUrl } from "@/utils";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ nfts }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const nftCollectionContext = useContext(NFTCollectionContext);
 
   return (
@@ -40,3 +44,23 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<{
+  nfts: NftType[];
+}> = async () => {
+  const nftsRes = await jsonRpsServices.fetchNfts();
+  const nfts: NftType[] = await Promise.all(
+    nftsRes.map((item) =>
+      axios.get(ipfsUriToUrl(item.uri)).then(({ data: metadata }) => {
+        return {
+          name: metadata.name,
+          description: metadata.description,
+          owner: item.owner,
+          id: item.id,
+          imageUrl: ipfsUriToUrl(metadata.image),
+        };
+      })
+    )
+  );
+  return { props: { nfts } };
+};
